665c870aae29bedb0c3ba4d505150eab27fcabfe
Philippe-Alexandre <pam@poum.ca>
2022-05-23 00:53:18 -0400

    Added configuration to link against OpenCV2

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+const char* IMG_PATH = "./images/apples.jpg";
+
 int main() {
-    return 0;
+    auto img = cv::imread(IMG_PATH);
+
+    cv::imshow("Original", img);
+    cv::waitKey();
 }
